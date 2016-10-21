8f37a1cf03ad6bd433b07be77615c3f2aa0a7785
sajnanidipen <sajnanidipen@gmail.com>
2016-10-21 09:13:18 +0530

    Add files via upload

---
+++ Lagest Number.py
@@ -0,0 +1,8 @@
+x=int(input("Enter the First Number:"))
+y=int(input("Enter the Second Number:"))
+if(x>y):
+      print("The value of",x ,"is greater than",y)
+if(x<y):
+      print("The value of",y ,"is greater than",x)
+if(x==y):
+      print("The value of",x," is equal to",y)
